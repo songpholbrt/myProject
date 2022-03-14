2c2b5045fc62d84f49cab5789d94cf092dc4dc69
Songphol Aiyarakom <songphol.brt@gmail.com>
2022-03-14 16:42:42 +0700

    add equipment

--- src/app/layouts/content/content.component.ts
+++ src/app/layouts/content/content.component.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MainService } from 'src/app/services/main.service';
+import Swal from 'sweetalert2';
+declare const $: any;
 
-export class Equipment{
+/*export class Equipment{
   constructor(
     public id: number,
     public code: string,
@@ -12,7 +14,7 @@ export class Equipment{
     public userid: number,
     public username: string,
   ) { }
-}
+}*/
 
 @Component({
   selector: 'app-content',
@@ -21,7 +23,17 @@ export class Equipment{
 })
 export class ContentComponent implements OnInit {
 
-  equipments!: Equipment[];
+  //equipments!: Equipment[];
+  public equ = new Equipment();
+  public equipments: any;
+  public equName = [
+    'Computer' , 'Printer', 'Scanner'
+  ];
+
+  errCode: boolean = false;
+  errDateAdd: boolean = false;
+  //errUsername: boolean = false;
+
 
   //public equipment: any;
   //public res: any;
@@ -35,29 +47,9 @@ export class ContentComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-
-    /*this.main.getData(this.urlEquip).subscribe(res => {
-      this.result = res;
-      //console.log(res);
-   });
-   */
-    
     this.getEquipment();
 
   }
-
-  /*
-  getEquipment() {
-    //this.main.get('equipmant').then((res: any) => {
-    //console.log(res);
-    //});
-
-    console.log(this.equipment);
-    this.equipment = this.res.data;
-
-    //localStorage.setItem("name", "John Doe");
-  }
-  */
   
   getEquipment() {
     this.httpClinet.get<any>(this.urlEquip).subscribe(
@@ -68,5 +60,80 @@ export class ContentComponent implements OnInit {
     );
   }
 
+  addEqipment() {
+    //alert("add");
+  }
+
+  editEqipment() {
+    alert("edit");
+  }
+
+  deleteEqipment() {
+    alert("delete");
+  }
+
+  onCode(str:string) {
+    //return this.errCode = (!str) ? true : false;
+    if (!str) {
+      this.errCode = true;
+    } else {
+      this.errCode = false;
+    }
+  }
+  onDateAdd(str:string) {
+    return this.errDateAdd = (!str) ? true : false;
+  }
+
+  onError() {
+    const arrError = [];
+    arrError.push(this.onCode(this.equ.code));
+    arrError.push(this.onDateAdd(this.equ.date_add));
+    return this.main.in_array(true, arrError);
+  }
+
+  onCancel() {
+    $('#modal-add-equipment').modal('hide');
+    this.equ = new Equipment();
+    this.errCode = false;
+    this.errDateAdd = false;
+    //alert("cancel");
+  }
+
+  onSubmit() {
+    //console.log(this.equ);
+
+    if (!this.onError()) {
+      //alert("adddd");
+      this.main.post('equipment', this.equ).then((res: any) => {
+        this.getEquipment();
+
+        Swal.fire({
+          position: 'top-end',
+          title: 'ยินดีด้วย!',
+          text: 'บันทึกข้อมูลสำเร็จ',
+          icon: 'success',
+          showConfirmButton: false,
+          timer: 1500,
+          allowOutsideClick: false
+        });
+
+      })
+
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'เกิดข้อผิดพลาด',
+        text: 'คุณกรอกข้อมูลไม่ถูกต้องหรือยังไม่สมบูรณ์ กรุณาตรวจสอบ',
+        allowOutsideClick: false
+      });
+    }
+  }
+
 }
 
+export class Equipment{
+  code: string = '0005';
+  name: string = 'Computer';
+  date_add: any = '2022-03-14';
+  username: string = '5';
+}
